9a7be7184d84dae26b5b47ede51a4e0c6a8df139
Nosa <egharevbanosa58@gmail.com>
2025-11-07 23:16:19 -0900

    Add scroll-to-top button functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// // Get the button element
+// const toTopBtn = document.getElementById("toTopBtn");
+
+// // Function to show/hide the button
+
+// // Check if the user has scrolled down 100 pixels
+// function scrollFunction() {
+//     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//         toTopBtn.classList.add("show"); // Add 'show' class to display the button
+//     } else {
+//         toTopBtn.classList.remove("show"); // Remove 'show' class to hide the button
+//     }
+// }
+
+// window.onscroll = function () {
+//     scrollFunction();
+// };
